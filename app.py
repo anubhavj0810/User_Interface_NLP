cdd9ce87bca3f787a3bac63023124de2a8c4b48e
anubhavj0810 <anubhavj0810@gmail.com>
2019-11-09 02:30:00 +0530

    Done some changes

--- app.py
+++ app.py
@@ -1,1 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def mainpage():
+    return render_template("mainpage.html")
+
+@app.route("/index")
+def index():
+	return "Hello, world!"
+
+@app.route("/<string:name>")
+def hello(name):
+	return f"Hello, {name}!"
+
+# @app.route("/graph")
+# def graph():
+
+# @app.route("/title_show", methods=["GET","POST"])
+# def title_show():
+
+if __name__ == '__main__':
+	app.run(debug=True)
