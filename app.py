870271fe5bafdd3cc45f5f5e2d2e802570c5befc
Chandan Maji <chandan.maji@iitgn.ac.in>
2019-11-10 03:52:34 +0530

    update frontend

--- app.py
+++ app.py
@@ -6,13 +6,13 @@ app = Flask(__name__)
 def mainpage():
     return render_template("mainpage.html")
 
-@app.route("/index", methods=["GET","POST"])
-def index():
-	return "Hello, world!"
+# @app.route("/index", methods=["GET","POST"])
+# def index():
+# 	return "Hello, world!"
 
-@app.route("/hello", methods=["POST"])
-def hello():
-	return "Hello"
+# @app.route("/hello", methods=["POST"])
+# def hello():
+# 	return "Hello"
 
 # @app.route("/graph")
 # def graph():
